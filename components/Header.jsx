231ad3442d25905d5a02ae747d19a2641808055a
Abdul Moid <abdulmoid337@gmail.com>
2025-02-04 23:39:34 +0530

    changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,6 @@ import { ShoppingCart, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import {
-  ClerkProvider,
   SignInButton,
   SignedIn,
   SignedOut,
